dee79a7b8aec7090962a8bb07e93ab56dc2e2b52
Nelio Alves <acenelio@gmail.com>
2018-02-13 17:04:30 -0200

    Delecao de usuario com DELETE

--- src/main/java/com/nelioalves/workshopmongo/services/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -32,6 +32,11 @@ public class UserService {
 		return repo.insert(obj);
 	}
 	
+	public void delete(String id) {
+		findById(id);
+		repo.delete(id);
+	}
+	
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
