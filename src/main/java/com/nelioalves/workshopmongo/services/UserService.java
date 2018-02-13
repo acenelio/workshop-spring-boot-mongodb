a7e888a9b15b99acb51f0db48204e41b69a63fc8
Nelio Alves <acenelio@gmail.com>
2018-02-13 18:17:43 -0200

    Atualizacao de usuario com PUT

--- src/main/java/com/nelioalves/workshopmongo/services/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -37,6 +37,17 @@ public class UserService {
 		repo.delete(id);
 	}
 	
+	public User update(User obj) {
+		User newObj = repo.findOne(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
